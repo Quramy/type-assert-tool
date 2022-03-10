7e01151297dd790fbc8efc709c8826573739daaf
Quramy <yosuke.kurami@gmail.com>
2022-03-10 14:55:57 +0900

    fix: AssertIs

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 interface Success { readonly __brand: unique symbol, readonly ok: true };
 export type AssertSub<T1, T2, M = [actual_type: T1, is_not_assignable_to: T2]> = T1 extends T2 ? Success: M;
-export type AssertIs<T1, T2, M = [actual_type: T1, is_not: T2]> = T1 extends T2 ? T2 extends T1 ? Success: M : M;
+export type AssertIs<T1, T2, M = [actual_type: T1, is_not: T2]> = AssertSub<T1, T2, M> extends Success ? AssertSub<T2, T1, M> : M;
 type AssertSuccess = TestSuite | Success;
 export type TestSuite = { readonly [caseName: string]: AssertSuccess };
